a2f784146d9c5664d571f16d65da59f5b1d043ca
goreleaserbot <goreleaser@carlosbecker.com>
2019-11-09 00:40:29 +0100

    Brew formula update for ssh2iterm2 version 0.7.1

--- Formula/ssh2iterm2.rb
+++ Formula/ssh2iterm2.rb
@@ -2,12 +2,12 @@
 class Ssh2iterm2 < Formula
   desc "Create iTerm2 dynamic profile from SSH config"
   homepage "https://github.com/arnested/ssh2iterm2"
-  version "0.7.0"
+  version "0.7.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/arnested/ssh2iterm2/releases/download/0.7.0/ssh2iterm2_0.7.0_darwin_amd64.tar.gz"
-    sha256 "7ea5db9ec705fd9cff47c8249eb107dbaa5c32b994e78ba2edf3b3b35e2e7de9"
+    url "https://github.com/arnested/ssh2iterm2/releases/download/0.7.1/ssh2iterm2_0.7.1_darwin_amd64.tar.gz"
+    sha256 "5228709a187e6096a36099e66f5eb737fc1b651d4423933b6a542473efa2d11d"
   elsif OS.linux?
   end
 
