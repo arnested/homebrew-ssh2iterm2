f8e59e682eeaecf55681fee9ac762ed9b379f9b4
goreleaserbot <goreleaser@carlosbecker.com>
2022-02-25 08:30:26 +0100

    Brew formula update for ssh2iterm2 version v1.0.32

--- Formula/ssh2iterm2.rb
+++ Formula/ssh2iterm2.rb
@@ -5,13 +5,13 @@
 class Ssh2iterm2 < Formula
   desc "Create iTerm2 dynamic profile from SSH config"
   homepage "https://github.com/arnested/ssh2iterm2"
-  version "1.0.31"
+  version "1.0.32"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/arnested/ssh2iterm2/releases/download/v1.0.31/ssh2iterm2_1.0.31_darwin_amd64.tar.gz"
-      sha256 "a7824304b0004a01b334f864fbd45fe28bad33f54f852d1362eab0a10a85d0df"
+      url "https://github.com/arnested/ssh2iterm2/releases/download/v1.0.32/ssh2iterm2_1.0.32_darwin_amd64.tar.gz"
+      sha256 "a9d04543269b787a985d41cb64c3f34717174a3fe6f2734fafd74c1bacd46cc5"
 
       def install
         bin.install "ssh2iterm2"
